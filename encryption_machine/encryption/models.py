1a795b94251cc5d6e2c1e43e0bd81b95fca8238f
Etozheigor <itshenko.io@gmail.com>
2023-06-12 11:27:09 +0300

    encryption model added

---
+++ encryption_machine/encryption/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from users.models import User
+
+
+class Encryption(models.Model):
+    """Модель шифрования."""
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='encryptions')
+    text = models.TextField(max_length=2000)
+    encryption_algorithm = models.CharField(max_length=100)
+    key = models.CharField(max_length=100, null=True)
+    is_encryption = models.BooleanField() # True, если шифруем, False, если дешифруем
+
+    class Meta:
+        verbose_name = 'Шифрование'
+        verbose_name_plural = 'Шифрования'
+
+    def encrypt_aes(text, key):
+        pass
+
+    def decrypt_aes(text, key):
+        pass
+
+    def encrypt_caesar(text, key):
+        pass
+
+    def decrypt_caesar(text, key):
+        pass
+
+    def encrypt_dsa(text, key):
+        pass
+
+    def decrypt_dsa(text, key):
+        pass
+
+    def encrypt_morse(text):
+        pass
+
+    def decrypt_morse(text):
+        pass
+
+    def encrypt_qr(text):
+        pass
+
+    def decrypt_qr(text):
+        pass
+
+    def encrypt_vigenere(text, key):
+        pass
+
+    def decrypt_vigenere(text, key):
+        pass
