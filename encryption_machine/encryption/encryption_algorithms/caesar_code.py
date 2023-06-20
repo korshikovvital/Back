c9956884255db883534ec0529527e933a0f30423
Dmitriy8854 <dmit.morozoff2009@yandex.ru>
2023-06-20 16:51:31 +0300

    New cript Caesar

--- encryption_machine/encryption/encryption_algorithms/caesar_code.py
+++ encryption_machine/encryption/encryption_algorithms/caesar_code.py
@@ -68,3 +68,58 @@ user_key = int(input('Введите любое число\n'))
 print(cipher_encrypt(user_text, user_key))
 # дешифрование
 print(cipher_decrypt(user_text, user_key))
+
+
+
+# Новый код шифра Цезаря из одной функции
+def encryption_mixin(text, key, chifr):
+    final_string = ""
+    for symbol in text:
+
+        if symbol.isupper():
+            
+            symbol_index = ord(symbol) + ord('А')
+            if chifr == 'Расшифровать':
+            # sсдвинуть текущий символ влево на позицию клавиши, чтобы получить его исходное положение
+                symbol_position = (symbol_index - key) % 32 + ord('А')
+            elif chifr == 'Зашифровать':
+                symbol_position = (symbol_index + key) % 32 + ord('А')
+            symbol_new = chr(symbol_position)
+            final_string += symbol_new
+
+        elif symbol.islower():
+            symbol_index = ord(symbol) - ord('а')
+            if chifr == 'Расшифровать':
+                symbol_position = (symbol_index - key) % 32 + ord('а')
+            elif chifr == 'Зашифровать':
+                symbol_position = (symbol_index + key) % 32 + ord('а')
+            symbol_new = chr(symbol_position)
+            final_string += symbol_new
+        elif symbol.isdigit():
+            # если это число, сдвиньте его фактическое значение
+            if chifr == 'Расшифровать':
+                symbol_index = (int(symbol) - key) % 10
+            elif chifr == 'Зашифровать':
+                symbol_index = (int(symbol) + key) % 10
+            final_string += str(symbol_index)
+        elif ord(symbol) >= 32 and ord(symbol) <= 47:
+            # если это число,4 сдвинуть его фактическое значение
+            symbol_index = ord(symbol) - ord(' ')
+            if chifr == 'Расшифровать':
+                symbol_position = (symbol_index - key) % 15 + ord(' ')
+            elif chifr == 'Зашифровать':
+                symbol_position = (symbol_index + key) % 15 + ord(' ')
+            symbol_new = chr(symbol_position)
+            final_string += symbol_new
+
+        else:
+            # если нет ни алфавита, ни числа, оставьте все как есть
+            final_string += symbol
+    return final_string
+
+user_text = input('Введите любой текст на русском\n')
+user_key = int(input('Введите любое число\n'))
+user_chifr = input('Введите "Расшифровать" или "Зашифровать" \n')
+
+# шифрование
+print(encryption_mixin(user_text, user_key, user_chifr))
