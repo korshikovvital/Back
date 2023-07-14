a4db41332125ffef2b2f9e5bc963b98caa305c2c
Etozheigor <itshenko.io@gmail.com>
2023-07-14 20:43:37 +0300

    update develop from main

--- encryption_machine/encryption_machine/settings.py
+++ encryption_machine/encryption_machine/settings.py
@@ -99,15 +99,9 @@ WSGI_APPLICATION = 'encryption_machine.wsgi.application'
 # }
 
 DATABASES = {
-<<<<<<< HEAD
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "db.sqlite3",
-=======
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
->>>>>>> 766e8ea5fefb3fa053d847c16236547bd1cf687f
     }
 }
 
